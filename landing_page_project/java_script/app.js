7758a175bfda3a210b2b1db3694418e49760f123
Angel <a.demerdzhiev@gmail.com>
2020-10-23 13:33:03 +0300

    scroll funtionality finished

--- landing_page_project/java_script/app.js
+++ landing_page_project/java_script/app.js
@@ -1,1 +1,35 @@
- 
+function scroll(element, duration) {
+  let elementQ = document.querySelector(element);
+  let elementPositionTop = elementQ.getBoundingClientRect().top;
+  let startPosition = window.pageYOffset;
+  let distance = elementPositionTop - startPosition;
+  let startTime =  null;
+
+  function animation(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+
+    let elapsedTime = currentTime - startTime;
+    let run = easeFunc(elapsedTime, startPosition, distance, duration);
+    window.scrollTo(0, run - 100);
+
+    if (elapsedTime < duration) {
+      requestAnimationFrame(animation);
+    }
+  }
+
+  function easeFunc(t, b, c, d) {
+	   t /= d;
+	   return c*t*t + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+let workButton = document.querySelector('#work-button')
+// let workSection = document.querySelector('#work-section');
+
+workButton.addEventListener('click', function() {
+  scroll('.work', 1000);
+});
