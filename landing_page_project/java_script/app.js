cef441ae2c4c5d617e6d6d8cc066a10b6e85df0c
Angel <a.demerdzhiev@gmail.com>
2020-10-28 13:47:50 +0200

    1. active section highlighting functionality added /n 2. dynamic navigation menu building functionality added

--- landing_page_project/java_script/app.js
+++ landing_page_project/java_script/app.js
@@ -1,29 +1,36 @@
+const sections = document.querySelectorAll("section");
+const navbar = document.querySelector('#navbar-ul');
+
+//function for dynamic building of the navigation menu
+function buildNavbar() {
+  const elementLi = document.createDocumentFragment();
+  for (const section of sections) {
+    let navbarItem = document.createElement('li');
+    const id = section.getAttribute('id');
+    const dataNav = section.getElementsByTagName("h1")[0].innerHTML;
+    navbarItem.innerHTML = `<a href="#${id}">${dataNav}</a>`;
+    elementLi.appendChild(navbarItem);
+  }
+
+  navbar.appendChild(elementLi);
+}
+
+buildNavbar();//calling the navigation menu build function
+
+//CODE FOR SMOOTH SCROLL SECTION
+
 const navbarButton = document.querySelector(".navbar-button");
 const navbarMenu = document.querySelector(".navbar ul");
 const navbarLinks = document.querySelectorAll(".navbar a");
-const navbarActiveMenu = document.querySelector(".navbar ul li a")
-navbarButton.addEventListener("click", navbarButtonClick);
 
+navbarButton.addEventListener("click", navbarButtonClick);
 function navbarButtonClick() {
   navbarButton.classList.toggle("open-navbar-button");
   navbarMenu.classList.toggle("open");
 }
 
-function activeSectionScroll(event) {
-  event.preventDefault();
-  const targetId = event.currentTarget.getAttribute("href");
-  const targetPostion = document.querySelector(targetId).offsetTop;
-  let scrollPostion = body.scrollTop;
-  console.log(scrollPosition)
-  if (scrollPostion >= targetPostion) {
-    activeSection;
-  }
-}
-navbarActiveMenu('scroll', activeSectionScroll);
-
 for(let i=0; i<navbarLinks.length; i++) {
   navbarLinks[i].addEventListener("click", navbarLinkClick);
-  navbarLinks[i].addEventListener("scroll", activeSectionScroll);
 }
 
 function navbarLinkClick(event) {
@@ -49,8 +56,36 @@ function activeSection () {
   navbarActiveMenu.classList.toggle("active");
 }
 
+//CODE FOR ACTIVE SCROLL SECTION
+//boolean function testing whether an element is in the viewport or not
+function isInViewport(element) {
+  const position = element.getBoundingClientRect();
+  if (position.top <= 100 && position.left >= 0 && position.bottom >= 90 &&
+  position.right <= (window.innerWidth ||
+    document.documentElement.clientWidth)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// if (position >= target) {
-//          $('#navigation > ul > li > a').removeClass('active');
-//          $('#navigation > ul > li > a[href=#' + id + ']').addClass('active');
-//      }
+// adding event listener to the scroll into sections
+document.addEventListener('scroll', function activeSection() {
+  for (const section of sections) {
+
+    if (isInViewport(section)) {
+
+      for (link of navbarLinks) {
+        if (link.getAttribute('href') == ('#' + section.getAttribute('id'))) {
+          link.classList.add('active');
+        }
+      }
+    } else {
+      for (link of navbarLinks) {
+        if (link.getAttribute('href') == ('#' + section.getAttribute('id'))) {
+            link.classList.remove('active');
+        }
+      }
+    }
+  }
+});
